aa06432ff4fb32e43ffedcd1e59171733135dbc1
HaHaDragon <thomas.toft@hotmail.com>
2014-08-23 12:27:31 +0200

    Added car acceleration

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,9 @@
 /// <reference path="../lib/phaser.d.ts" />
 module states {
     export class Player extends Phaser.Sprite {
+        MAX_SPEED: number = 10;
+        ROTATION_SPEED: number = 5;
+        
         constructor(game: Phaser.Game, x: number, y: number) {
             super(game, x, y, "car", 0);
             this.anchor.setTo(0.5, 0.5);
@@ -9,28 +12,35 @@ module states {
             this.game.physics.p2.enableBody(this, false);
             
             var body: Phaser.Physics.P2.Body = this.body;
-            body.setRectangle(48, 32);            
+            body.setRectangle(48, 32);
             game.add.existing(this);
         }
         
         update() 
         {
-            this.animations.frame = 0;
-            this.body.velocity.x = 0;
-            this.body.velocity.y = 0;
-            this.body.angularVelocity = 0;
-            if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) 
+            if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT) || this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
             {
-                this.body.angularVelocity = -5;
+                if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) 
+                {
+                    this.body.angularVelocity = -this.ROTATION_SPEED;
+                }
+                if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) 
+                {
+                    this.body.angularVelocity = this.ROTATION_SPEED;
+                }
             }
-            if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) 
+            else
             {
-                this.body.angularVelocity = 5;
+                this.body.angularVelocity = 0;
             }
             if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
             {
-                this.body.velocity.x = Math.cos(this.rotation) * 150;
-                this.body.velocity.y = Math.sin(this.rotation) * 150;
+                this.body.damping = 0.0;
+                this.body.applyForce([Math.cos(this.rotation) * -this.MAX_SPEED, Math.sin(this.rotation) * -this.MAX_SPEED], this.x, this.y);
+            }
+            else
+            {
+                this.body.damping = 0.7;
             }
         }
     }
