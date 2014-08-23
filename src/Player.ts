c47dc1bcdc16ced1fb6dbf8f6bda067a7ce32f3a
HaHaDragon <thomas.toft@hotmail.com>
2014-08-23 10:51:25 +0200

    Added car controls

--- src/Player.ts
+++ src/Player.ts
@@ -1,34 +1,32 @@
 /// <reference path="../lib/phaser.d.ts" />
 module states {
     export class Player extends Phaser.Sprite {
-        
         constructor(game: Phaser.Game, x: number, y: number) {
-            super(game, x, y, "simon", 0);
-            this.anchor.setTo(0.5, 0);
+            super(game, x, y, "car1", 0);
+            this.anchor.setTo(0.5, 0.5);
             this.animations.add("walk", [0, 1, 2, 3, 4], 10, true);
             game.physics.arcade.enableBody(this);
             game.add.existing(this);
         }
         
-        update() {
+        update() 
+        {
+            this.animations.frame = 0;
             this.body.velocity.x = 0;
-            
-            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-                this.body.velocity.x = -150;
-                this.animations.play("walk");
-                
-                if (this.scale.x == 1) {
-                    this.scale.x = -1;
-                }
-            } else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-                this.body.velocity.x = 150;
-                this.animations.play("walk");
-                
-                if (this.scale.x == -1) {
-                    this.scale.x = 1;
-                }
-            } else {
-                this.animations.frame = 0;
+            this.body.velocity.y = 0;
+            this.body.angularVelocity = 0;
+            if(this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) 
+            {
+                this.body.angularVelocity = -200;
+            }
+            if(this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) 
+            {
+                this.body.angularVelocity = 200;
+            }
+            if(this.game.input.keyboard.isDown(Phaser.Keyboard.UP))
+            {
+                this.body.velocity.x = Math.cos(this.rotation) * 50;
+                this.body.velocity.y = Math.sin(this.rotation) * 50;
             }
         }
     }
