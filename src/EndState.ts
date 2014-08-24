97f43640a6af27a218baab216555cff8000655bf
pedersen <jtp@cs.au.dk>
2014-08-24 17:11:32 +0200

    emit cars

--- src/EndState.ts
+++ src/EndState.ts
@@ -7,18 +7,26 @@ module states {
         background: Phaser.Sprite;
         logo: Phaser.Sprite;
 	ps : PlayState;
+	emitter: Phaser.Particles.Arcade.Emitter;
 	
 	constructor(ps : PlayState) {
 	    super();
 	    this.ps = ps;
 	}
         create() {
-            this.background = this.add.sprite(0, 0, "dead");
-            this.background.alpha = 0;
-            
-            this.add.tween(this.background).to({ alpha: 1}, 2000, Phaser.Easing.Elastic.InOut, true);
             this.input.onDown.addOnce(this.fadeOut, this);
+
 	    
+	    this.emitter = this.ps.game.add.emitter(this.ps.game.width/2, 50, 1000);            
+            this.emitter.makeParticles("car");
+            this.emitter.gravity = 50;
+            //this. emitter.setScale(0.3, 2, 0.3, 2, 1000, Phaser.Easing.Cubic.InOut, false);  
+            this.emitter.setAlpha(0.25, 1.0, 2000);
+            this.emitter.setXSpeed(-25, 25);
+            this.emitter.setYSpeed(-25, 25);
+	    this.emitter.start(false, 100000, null, 100);
+
+
             this.createText(20, 100, "#00FF00", 24, "you connected " + (this.ps.player.housesConnected) + " houses");
             this.createText(20, 200, "#00FF00", 24, "You scored: " + (this.ps.player.score));
             this.createText(20, 300, "#00FF00", 24, "You played for: " + Math.floor((this.ps.game.time.totalElapsedSeconds() - this.ps.player.gameStarted)) + " seconds");
@@ -28,7 +36,7 @@ module states {
 
 	update() {
 	    // if (this.game.input.keyboard.isDown(Phaser.Keyboard.ANY)) fadeOut();
-
+	    this.emitter.x  = (this.emitter.x + 13) % this.ps.game.width;
 	}
 
 	createText(x: number, y: number, color: Phaser.Color, size: number, text: string)  {
