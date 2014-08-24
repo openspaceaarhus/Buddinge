f247e5e30bf18afeae0b64d1c7fae8ca7b923c1b
Rene Hangstrup Møller <rhmoller@gmail.com>
2014-08-24 11:21:22 +0200

    fixed motor sound on firefox

--- src/PlayState.ts
+++ src/PlayState.ts
@@ -108,9 +108,9 @@ module states {
             this.game.load.image("asphalt", "assets/asphalt.png");
 	       this.game.load.image("hep", "assets/hep.png");
             
+            this.game.load.audio("motorsound", "assets/sound/sound_motor.wav");
             this.game.load.audio("ding", "assets/sound/sound_haleding.wav");
             this.game.load.audio("collide", "assets/sound/sound_kollision.wav");
-            this.game.load.audio("motorsound", "assets/sound/sound_motor.wav", true);
             //this.game.load.audio("motorstrained", "assets/sound/sound_motorbelastet.wav");
         }
         
